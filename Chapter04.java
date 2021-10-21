e74606406fad7b5e112f95b425f9202b596f8bb7
maruyu31 <you3746336@gmail.com>
2021-10-21 14:26:27 +0900

    [Finish] chapter04

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		name = "Yamada Taro";
+		System.out.println(name);
+		
+		name = "My Favorite Word!";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
