01fcee6de5596243102d937a1af78ae035906468
maruyu31 <you3746336@gmail.com>
2021-10-21 14:50:31 +0900

    [Finish] chapter05

---
+++ Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 10010101L;
+		double double2 = 3.14159d;
+		boolean bool3 = true;
+		System.out.println(long1);
+		System.out.println(double2);
+		System.out.println(bool3);
+		
+		Float float1 = 1.41421356f;
+		Integer int2 = 1515;
+		Character char3 = 'a';
+		System.out.println(float1);
+		System.out.println(int2);
+		System.out.println(char3);
+		
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array1[2]);
+	}
+}
